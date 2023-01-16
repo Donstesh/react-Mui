41bc076adefb432620d3c5045eef27f27c8273ea
Donstesh <stephenshifoko@gmail.com>
2023-01-16 18:36:09 +0300

    paragraphs

--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -13,6 +13,9 @@ export const MuiTypography = () => {
 
         <Typography variant='subtitle1'> Sub Title 1.</Typography>
         <Typography variant='subtitle2'> Sub Title 2.</Typography>
+
+        <Typography variant='body1'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque praesentium, numquam odit aperiam autem dicta, hic error recusandae quis iure aliquam mollitia vel qui possimus ab neque pariatur maiores ipsum..</Typography>
+        <Typography variant='body2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima blanditiis totam reiciendis corporis illum nam veritatis dolor sint, delectus eligendi magnam similique unde nemo rem. Eius optio officiis animi..</Typography>
     </div>
   )
 }
