3d4663b83cd90d48ef24dce242739d925cb22a90
Donstesh <stephenshifoko@gmail.com>
2023-01-16 18:42:15 +0300

    adding bottom space using gutterBottom

--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -7,7 +7,7 @@ export const MuiTypography = () => {
         <Typography variant='h1'> H1 Heading.</Typography>
         <Typography variant='h2'> H2 Heading.</Typography>
         <Typography variant='h3'> H3 Heading.</Typography>
-        <Typography variant='h4'> H4 Heading.</Typography>
+        <Typography variant='h4' component='h1' gutterBottom> H4 Heading.</Typography>
         <Typography variant='h5'> H5 Heading.</Typography>
         <Typography variant='h6'> H6 Heading.</Typography>
 
