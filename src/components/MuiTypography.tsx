dbbe265d943c8df8f54c4e72607bf44927503d2d
Donstesh <stephenshifoko@gmail.com>
2023-01-16 18:26:01 +0300

    Headings

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'> H1 Heading.</Typography>
+        <Typography variant='h2'> H2 Heading.</Typography>
+        <Typography variant='h3'> H3 Heading.</Typography>
+        <Typography variant='h4'> H4 Heading.</Typography>
+        <Typography variant='h5'> H5 Heading.</Typography>
+        <Typography variant='h6'> H6 Heading.</Typography>
+    </div>
+  )
+}
